b0db3f638dc1730da5abac5dc7e9fbb90c53cb75
André Abrahão <abrahaoam@gmail.com>
2023-04-17 17:44:49 -0300

    Modulo 2

---
+++ atualizando-lista.js
@@ -0,0 +1,8 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+            //posicao 
+nomes.splice(1, 2, "Rodrigo"); //Adciona
+                //quantidade de exclusoes a direita.
+
+
+console.log(nomes);
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`A aluna da posicao 1 da lista de alunos e: ${listaDeAlunosEMedias[0][1]}, A nota dessa aluna e ${listaDeAlunosEMedias[1][1]}`)
