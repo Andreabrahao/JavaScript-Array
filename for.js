1fafdecfc58a221c17546e38aaa1e7887df806e6
André Abrahão <abrahaoam@gmail.com>
2023-04-24 11:24:48 -0300

    Tipos de for

---
+++ for.js
@@ -0,0 +1,9 @@
+const numero = [100, 200, 300, 400, 500, 600];
+
+// Primeira expressao: e executada apenas uma vez
+// Segunda expressao: e uma condicao e execucao
+// Terceira expressao: e executada sempre no final do bloco
+
+for (let indice = 0; indice < numero.length; indice++){
+    console.log(numero[indice]);
+}
---
+++ media-for-of.js
@@ -0,0 +1,11 @@
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+for(let elementos of notas){
+    somaDasNotas += elementos;
+}
+
+const media = somaDasNotas / notas.length;
+
+console.log(`A media das notas e ${media}`);
